a6f90ea675a88671bd4c2a9436308e1fd7df5c8d
dduckchul <dduckchul@naver.com>
2025-05-20 15:04:27 +0900

    feat: 모델 스케일 수정

--- Assets/08_Scripts/Player/PlayerBehavior.cs
+++ Assets/08_Scripts/Player/PlayerBehavior.cs
@@ -1,15 +1,21 @@
+using System;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerBehavior : MonoBehaviour
 {
     /* 플레이어의 상태 나타내는 변수 */
-    public bool isSitting;
-    public bool isGrabbing;
-    public bool isMoving;
+    private bool isSitting;
+    private bool isGrabbing;
+    private bool isMoving;
 
-    // Start is called before the first frame update
-    void Start()
+    public Camera playerCam;
+
+    public bool IsSitting => isSitting;
+    public bool IsGrabbing => isGrabbing;
+    public bool IsMoving => isMoving;
+
+    private void FixedUpdate()
     {
-
+        throw new NotImplementedException();
     }
 }
