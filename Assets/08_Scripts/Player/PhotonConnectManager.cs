bd3437f72fb80bb227b1f9f9533ea91cc7f034bd
CHM <190792910+Tajo-Nero@users.noreply.github.com>
2025-05-29 13:06:15 +0900

    feat: 태우리 RPC관련 완
    태우리 생존시간, 소백이 해야함

--- Assets/08_Scripts/Player/PhotonConnectManager.cs
+++ Assets/08_Scripts/Player/PhotonConnectManager.cs
@@ -62,7 +62,7 @@ public class PhotonConnectManager : MonoBehaviourPunCallbacks
         {
             PhotonNetwork.JoinOrCreateRoom(
                 _testRoomName,
-                new RoomOptions{ MaxPlayers = 6 },
+                new RoomOptions { MaxPlayers = 6 },
                 new TypedLobby(_testLobbyName, LobbyType.Default)
             );
         }
@@ -98,30 +98,17 @@ public class PhotonConnectManager : MonoBehaviourPunCallbacks
     }
 
     /* 테스트용 방 곧바로 입장시, 바로 플레이어 생성이후 XR 컴포넌트 켜줌. */
-    //public override void OnJoinedRoom()
-    //{
-    //    // 초
-    //    GameObject player = _playerSpawner.NetworkInstantiate(PlayerEnum.Jennie);
-    //    player.GetComponent<PlayerComponents>().xRComponents.SetActive(true);
-    //
-    //    GameManager.Instance.ResetGameTimer();
-    //    Debug.Log("나 참가 " + PhotonNetwork.LocalPlayer + "Room : " + PhotonNetwork.CurrentRoom.Name);
-    //}
-
-    //CHM  소백이 플레이어 찾아서 플레이어 카메라 따라다니기 
     public override void OnJoinedRoom()
     {
+        // 초
         GameObject player = _playerSpawner.NetworkInstantiate(PlayerEnum.Jennie);
-        PlayerComponents playerComponents = player.GetComponent<PlayerComponents>();
-        playerComponents.xRComponents.SetActive(true);
-
-        // 소백이 생성 (로컬 전용)
-        GameObject mySobaek = Instantiate(sobaekPrefab);
-        mySobaek.GetComponent<Sobaek>().SetOwner(playerComponents);
+        player.GetComponent<PlayerComponents>().xRComponents.SetActive(true);
 
         GameManager.Instance.ResetGameTimer();
         Debug.Log("나 참가 " + PhotonNetwork.LocalPlayer + "Room : " + PhotonNetwork.CurrentRoom.Name);
     }
+
+
 
     public override void OnJoinedLobby()
     {
