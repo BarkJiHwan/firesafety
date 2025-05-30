9c15877bdcc5b8e01e152916bd751463ade4872d
wooyou0044 <chawoo2206@gmail.com>
2025-05-30 14:02:06 +0900

    feat: MainScene CurvedMesh 완성

---
+++ Assets/08_Scripts/UI/CurvedUIRender.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurvedUIRender : MonoBehaviour
+{
+    [SerializeField] LayerMask UILayer;
+
+    [SerializeField] RenderTexture outputTexture;
+    [SerializeField] Camera UICamera;
+    [SerializeField] Canvas canvas;
+    [SerializeField] GameObject meshSurface;
+
+    Vector2 renderSize;
+    void Start()
+    {
+        renderSize = new Vector2(1024,512);
+        SetRenderTextureUI();
+    }
+
+    void SetRenderTextureUI()
+    {
+        outputTexture = new RenderTexture((int)renderSize.x, (int)renderSize.y, 16);
+        outputTexture.antiAliasing = 4;
+        outputTexture.name = "CurvedUI_RenderTexture";
+        outputTexture.useMipMap = false;
+        outputTexture.filterMode = FilterMode.Bilinear;
+        outputTexture.Create();
+
+        GameObject camObj = new GameObject("UI Render Camera");
+        // 카메라 Y축 위치 수정
+        camObj.transform.position = new Vector3(0, 0.9f, -10);
+        UICamera = camObj.AddComponent<Camera>();
+        UICamera.clearFlags = CameraClearFlags.SolidColor;
+        UICamera.backgroundColor = Color.gray;
+        UICamera.backgroundColor = new Color(0, 0, 0, 0);
+        UICamera.cullingMask = UILayer;
+        UICamera.orthographic = true;
+        UICamera.orthographicSize = 0.8f;
+        UICamera.targetTexture = outputTexture;
+
+        canvas.worldCamera = UICamera;
+        meshSurface.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        if(meshSurface != null)
+        {
+            var mr = meshSurface.GetComponent<MeshRenderer>();
+            var mat = new Material(Shader.Find("Unlit/Transparent"));
+            mat.mainTexture = outputTexture;
+            mr.material = mat;
+        }
+    }
+
+    int LayerMaskToLayer(int layerMask)
+    {
+        for(int i=0; i<32; i++)
+        {
+            if ((layerMask & (1 << i)) != 0)
+                return i;
+        }
+        return 0;
+    }
+}
