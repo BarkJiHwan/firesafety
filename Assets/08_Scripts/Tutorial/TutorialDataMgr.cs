d6c70d78a628e68d4f07a91cd7bd5742cb710386
BarkJiHwan <wlghksdl159@naver.com>
2025-06-11 20:09:28 +0900

    feat: 튜토리얼 수정 완료
    튜토리얼 기획서의 수정 내용 반영 완료

--- Assets/08_Scripts/Tutorial/TutorialDataMgr.cs
+++ Assets/08_Scripts/Tutorial/TutorialDataMgr.cs
@@ -9,12 +9,18 @@ public class TutorialDataMgr : MonoBehaviourPun
     [field: Header("플레이어별 상호작용 오브젝트 (0~5번)")]
     [field: SerializeField]
     public List<GameObject> InteractObjects { get; set; }
-    public bool _tutorialCompleted;
+
     [Header("플레이어별 튜토리얼 데이터 (0~5번)")]
     [SerializeField] private TutorialData[] allPlayerData;
 
     [Header("기본 데이터 (에러 발생 시 사용)")]
     [SerializeField] private TutorialData fallbackData;
+
+    private Coroutine _tutorialRoutine;
+
+    public bool IsStartTutorial { get; set; }
+    public bool IsTutorialFailed { get; set; }
+    public float Timer { get; private set; } = 90f;
 
     void Awake()
     {
@@ -24,7 +30,8 @@ public class TutorialDataMgr : MonoBehaviourPun
             return;
         }
         Instance = this;
-        _tutorialCompleted = false;
+        IsStartTutorial = false;
+        IsTutorialFailed = false;
     }
 
     // PlayerList 인덱스 기반 데이터 반환
@@ -59,42 +66,23 @@ public class TutorialDataMgr : MonoBehaviourPun
     {
         if (PhotonNetwork.LocalPlayer.IsLocal)
         {
-            StartCoroutine(TutorialRoutine());
+            _tutorialRoutine = StartCoroutine(TutorialRoutine());
         }
     }
 
     private IEnumerator TutorialRoutine()
     {
-        float timer = 3f;
-        while (timer > 0f)
+        yield return new WaitUntil(() => IsStartTutorial);
+        float timer = 90f;
+        while (timer > 0)
         {
-            Debug.Log($"튜토리얼 준비까지: {timer:F1}초");
             timer -= Time.deltaTime;
             yield return null;
         }
-
-        timer = 90f;
-        while (timer > 0 && !_tutorialCompleted)
-        {
-            Debug.Log($"튜토리얼중 {timer:F1}초");
-            timer -= Time.deltaTime;
-            yield return null;
-        }
-        if(timer > 0f)
-        {
-            CompleteTutorial();
-        }
-        else
-        {
-            //시간 초과시 발생해야 하는 이벤트 등....을 여기서 추가하면 됨
-            CompleteTutorial();
-        }
+        TutorialFailed();
     }
-    public void IsTutorialComplete() => _tutorialCompleted = true;
 
-    public void CompleteTutorial()
-    {
-        Hashtable props = new Hashtable() { { "IsReady", true } };
-        PhotonNetwork.LocalPlayer.SetCustomProperties(props);
-    }
+    public void TutorialFailed() => IsTutorialFailed = true;
+
+    public void StopTutorialRoutine() => StopCoroutine(_tutorialRoutine);
 }
