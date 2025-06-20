35dd829cc9fac0ed124e165c16cedb9d2087e32c
Camembertkimchi <83268157+Camembertkimchi@users.noreply.github.com>
2025-06-20 15:20:34 +0900

    fix: RPC 관련 수정

--- Assets/08_Scripts/Interactable/SHE/FireSuppressantManager.cs
+++ Assets/08_Scripts/Interactable/SHE/FireSuppressantManager.cs
@@ -404,24 +404,40 @@ public class FireSuppressantManager : MonoBehaviourPunCallbacks
     [PunRPC]
     private void RPC_PlayInitialFX(EHandType type)
     {
+        if (!pView.IsMine)
+        {
+            return;
+        }
         var hand = GetHand(type);
         hand.initialFireFX.Play();
     }
     [PunRPC]
     private void RPC_PlayNormalFX(EHandType type)
     {
+        if (!pView.IsMine)
+        {
+            return;
+        }
         var hand = GetHand(type);
         hand.normalFireFX.Play();
     }
     [PunRPC]
     private void RPC_PlayZeroAmountFX(EHandType type)
     {
+        if (!pView.IsMine)
+        {
+            return;
+        }
         var hand = GetHand(type);
         hand.zeroAmountFireFX.Play();
     }
     [PunRPC]
     private void RPC_StopPlayFX(EHandType type)
     {
+        if (!pView.IsMine)
+        {
+            return;
+        }
         var hand = GetHand(type);
         if (hand.initialFireFX.isPlaying)
         {
@@ -439,6 +455,10 @@ public class FireSuppressantManager : MonoBehaviourPunCallbacks
     [PunRPC]
     private void RPC_SetActiveModel(EHandType type)
     {
+        if (!pView.IsMine)
+        {
+            return;
+        }
         var hand = GetHand(type);
         if (!hand.modelPrefab.activeSelf)
         {
@@ -448,6 +468,10 @@ public class FireSuppressantManager : MonoBehaviourPunCallbacks
     [PunRPC]
     private void RPC_SetActiveModelFalse()
     {
+        if (!pView.IsMine)
+        {
+            return;
+        }
         if (!_rightHand.enabled && _rightHand.modelPrefab.activeSelf)
         {
             _rightHand.modelPrefab.SetActive(false);
