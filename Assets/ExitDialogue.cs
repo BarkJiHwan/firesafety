2bed96cd21ec680b0529408f9864d1919c9d96f5
dduckchul <dduckchul@naver.com>
2025-06-26 12:08:39 +0900

    feat: 캐릭터 크기 조정 / 파티클 조정

--- Assets/ExitDialogue.cs
+++ Assets/ExitDialogue.cs
@@ -52,7 +52,7 @@ public class ExitDialogue : MonoBehaviour
         }
 
         // 4층 종료지점일때 퀴즈 끄기
-        if (other.gameObject.name.Equals("Floor4 WayPoints.End"))
+        if (other.gameObject.name.Equals("Floor4 WayPoints.End") && quizUI.gameObject.activeSelf)
         {
             OnSelectRightAnswer();
         }
